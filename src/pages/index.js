cbb063c79e74e72720d33dd361f9f09b54fc013d
Rhys <rbrooker105@gmail.com>
2017-11-13 20:43:48 +1100

    complete tutorial one

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
-import React from "react"
+import React from "react";
+import Link from "gatsby-link";
+require('./counter.js');
 
-export default () => <div>Hello world!</div>
+export default () => 
+<div style={{ color: `tomato` }}>
+   <h1>Hello Gatsby!</h1>
+   <p>What a world.</p>
+   <img src="http://lorempixel.com/400/200/" alt="" />
+   <br />
+   <Link to="/page-2/">Link</Link>
+   <Link to="/counter/">Counter</Link>
+ </div>
